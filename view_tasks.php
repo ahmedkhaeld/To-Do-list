629c16b661abee6975725f2b653526984ef53f42
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2021-08-12 14:22:59 +0200

    Feat Creat form that gives option to mark completed tasks
    Make use of heredoc syntax
    Check multiple conditions must met
    form is submitted, has a value, and non-empty array
    even if only one check box is selected would be and an array

--- view_tasks.php
+++ view_tasks.php
@@ -15,8 +15,10 @@ function make_list($parent, $tasks) {
     echo '<ol>'; // Start an ordered list.
     // Loop through each subarray:
     foreach($parent as $task_id =>$todo) {
-        // Display the item:
-        echo "<li>$todo";
+        // Start with a checkbox!
+        echo <<<EOT
+        <li><input type="checkbox" name="tasks[$task_id]" value="done"> $todo
+        EOT;
         // Check for subtasks:
         if(isset($tasks[$task_id])) {
             make_list($tasks[$task_id],$tasks);
@@ -25,6 +27,33 @@ function make_list($parent, $tasks) {
     } // End of FOREACH loop.
     echo '</ol>'; // Close the ordered list.
 } // End of make_list() function.
+
+
+// Check if the form has been submitted:
+    
+    if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['tasks'])
+            && is_array($_POST['tasks'])  && !empty($_POST['tasks'])) {
+            // Define the query:
+        $sql = 'UPDATE tasks SET date_completed=NOW() WHERE task_id IN (';
+        // Add each task ID:
+        foreach ($_POST['tasks'] as $task_id => $v) {
+         $sql .= $task_id . ', ';
+            
+        }
+            // Complete the query and execute:
+            $sql = substr($sql, 0, -2) . ')';
+            $result = mysqli_query($conn, $sql);
+            // Report on the results:
+            
+            if (mysqli_affected_rows($conn) == count($_POST['tasks'])) {
+            
+               echo '<p>The task(s) have been marked as completed!</p>';
+    
+            } else {
+
+               echo '<p>Not all tasks could be marked as completed!</p>';
+            }
+    } // End of submission IF.
 
 $sql='SELECT task_id, parent_id, task FROM tasks WHERE date_completed="0000-00-00
 00:00:00" ORDER BY parent_id, date_added ASC';
@@ -38,9 +67,21 @@ while (list($task_id, $parent_id, $task) = mysqli_fetch_array($result, MYSQLI_NU
   $tasks[$parent_id][$task_id] = $task;
 }
 
+// Make a form:
+    echo <<<EOT
+    <p>Check the box next to a task and click "Update" to mark a task as completed (it,
+    and any subtasks, will no longer appear in this list).</p>
+    <form action="view_tasks.php" method="post">
+    EOT;
 // Send the first array element
 // to the make_list() function:
 make_list($tasks[0],$tasks);
+
+// Complete the form:
+    echo <<<EOT
+    <input name="submit" type="submit" value="Update" />
+    </form>
+    EOT;
 ?>
 </body>
 </html>
